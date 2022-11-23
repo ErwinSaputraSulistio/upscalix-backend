9a959af157066bb90e3fc817d7e4dc9c96adb8d2
Erwin <erwin.saputra.sulistio@gmail.com>
2022-11-24 00:08:50 +0700

    Fix bug at logics

--- src/helpers/birthdayToJakartaTime.js
+++ src/helpers/birthdayToJakartaTime.js
@@ -1,18 +1,12 @@
 const birthdayToJakartaTime = (dateInput, gmt) => {
     try {
         const birthDayArray = dateInput.split("/")
-        const gmtZeroTime = 2 
+        const gmtZeroTime = 16
         const gmtNumber = parseInt(gmt.slice(0,3))
         let dateOfBirth = null
         let timeToSendMessages = null
-        if(gmtNumber < -2) {
-            dateOfBirth = `${parseInt(birthDayArray[0] - 1)}/${birthDayArray[1]}`
-            timeToSendMessages = (24 + gmtZeroTime) + gmtNumber
-        }
-        else {
-            dateOfBirth = birthDayArray[0] + "/" + birthDayArray[1]
-            timeToSendMessages = gmtZeroTime + gmtNumber
-        }
+        dateOfBirth = `${parseInt(birthDayArray[0])}/${birthDayArray[1]}`
+        timeToSendMessages = gmtZeroTime - gmtNumber
         return { date: dateOfBirth, time: timeToSendMessages }
     }
     catch(err) { throw new Error(err) }
